7fff91507344abe322e9787ddd1cf7a3b1a6dcc9
Gal Margalit <galmargalit@gmail.com>
2019-08-29 15:57:10 +0300

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import 'particles.js';
+import socialFacebook from '@iconify/icons-simple-line-icons/social-facebook';
+import socialSoundcloud from '@iconify/icons-simple-line-icons/social-soundcloud';
+import socialInstagram from '@iconify/icons-simple-line-icons/social-instagram';
+import socialYoutube from '@iconify/icons-simple-line-icons/social-youtube';
+import SVGAnim from "snapsvg-animator";
+import "snapsvg-cjs";
+import Iconify from '@iconify/iconify';
+
+import classes from './index.css';
+import particlesJSON from './particles.json';
+import logo from './logo.json';
+
+
+particlesJS("particles-js", particlesJSON );
+const container = document.getElementById('logo');
+const svg = new SVGAnim(logo, 200, 220, 40);
+container.appendChild(svg.s.node);
