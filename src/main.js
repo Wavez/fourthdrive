48b48efddd3e2207ec8a98ed9049c8cce6012e1b
Gal Margalit <galmargalit@gmail.com>
2025-05-23 12:28:42 +0300

    Switch to Vite from Parcel and remove unused features

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { tsParticles } from "@tsparticles/engine";
+// import { tsParticles } from "@tsparticles/engine";
+import './style.css'
 
 import socialFacebook from '@iconify/icons-simple-line-icons/social-facebook';
 import socialSoundcloud from '@iconify/icons-simple-line-icons/social-soundcloud';
@@ -6,9 +7,8 @@ import socialInstagram from '@iconify/icons-simple-line-icons/social-instagram';
 import socialYoutube from '@iconify/icons-simple-line-icons/social-youtube';
 import Iconify from '@iconify/iconify';
 
-import classes from './index.scss';
-import particlesJSON from './particles.json';
-import iconGlitch from './iconGlitch';
+// import particlesJSON from './particles.json';
+import iconGlitch from '../../vitejs-vite-h8eab5ze/src/iconGlitch';
 
 //fixme
 
