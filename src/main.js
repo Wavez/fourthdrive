d1435eccb97be40ab818dc749bdc8e3a77524728
Gal Margalit <galmargalit@gmail.com>
2025-05-23 18:16:00 +0300

    Remove tsParticles dependency and related assets

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// import { tsParticles } from "@tsparticles/engine";
 import './style.css'
 
 import socialFacebook from '@iconify/icons-simple-line-icons/social-facebook';
@@ -7,26 +6,4 @@ import socialInstagram from '@iconify/icons-simple-line-icons/social-instagram';
 import socialYoutube from '@iconify/icons-simple-line-icons/social-youtube';
 import Iconify from '@iconify/iconify';
 
-// import particlesJSON from './particles.json';
 import iconGlitch from './iconGlitch';
-
-//fixme
-
-// tsParticles.load("tsparticles", particlesJSON);
-
-/*
-tsParticles.load({
-    id: "tsparticles",
-    options: {
-        particles: {
-            shape: {
-                type: "square",
-            },
-        },
-        preset: "links",
-    },
-});
-
-*/
-
-
