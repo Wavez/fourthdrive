a5db3bf23cc053fa47f6bca9068df4cb70c16018
Gal Margalit <galmargalit@gmail.com>
2025-08-10 18:43:48 +0300

    Update social media links and icons in HTML - add feature.fm and Gumroad links with corresponding icons.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
 import './style.css'
-
-import socialFacebook from '@iconify/icons-simple-line-icons/social-facebook';
-import socialSoundcloud from '@iconify/icons-simple-line-icons/social-soundcloud';
-import socialInstagram from '@iconify/icons-simple-line-icons/social-instagram';
-import socialYoutube from '@iconify/icons-simple-line-icons/social-youtube';
 import Iconify from '@iconify/iconify';
-
 import iconGlitch from './iconGlitch';
